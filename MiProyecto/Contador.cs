4fb68d73ec1d467f72d4c56e08a77a3047103bfc
Mariana <casallasmariana912@gmail.com>
2026-03-19 21:40:22 -0500

    Cambios reto 1 y realiazación reto 2

--- MiProyecto/Contador.cs
+++ MiProyecto/Contador.cs
@@ -1,41 +1,29 @@
 using System;
 
-     public class Contador
-     {
-         private int valor;
+public class Contador
+{
+    private int valor;
 
-         public Contador()
-         {
-             valor = 0;
-         }
+    public Contador()
+    {
+        valor = 0;
+    }
 
-         public int Incrementar()
-         {
-            valor ++;
-             return valor;
-         }
+    public int Incrementar()
+    {
+        valor++;
+        return valor;
+    }
 
-         public int Decrementar()
-         {
-             valor --;
-             return valor;
-         }
+    public int Decrementar()
+    {
+        valor--;
+        return valor;
+    }
 
-         public int Resultado()
-         {
-             return valor;
-         }
-     }
-
-     public class Program
-     {
-         public static void Main()
-         {
-             Contador contador = new Contador();
-
-             Console.WriteLine("Valor inicial: " + contador.Resultado());
-             Console.WriteLine("Incrementar: " + contador.Incrementar());
-             Console.WriteLine("Decrementar: " + contador.Decrementar());
-         }
-     }
+    public int Resultado()
+    {
+        return valor;
+    }
+}
 
